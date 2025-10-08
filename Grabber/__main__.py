f203143e2568b649d2620a86958bc496c01d9f28
Shadow-rot <looktouhid@gmail.com>
2025-10-09 00:56:25 +0530

    Update __main__.py

--- Grabber/__main__.py
+++ Grabber/__main__.py
@@ -12,7 +12,7 @@ from telegram.ext import CommandHandler, CallbackContext, MessageHandler, filter
 
 from Grabber import collection, top_global_groups_collection, group_user_totals_collection, user_collection, user_totals_collection, shivuu
 from Grabber import application, SUPPORT_CHAT, UPDATE_CHAT, db, LOGGER
-from shivu.modules import ALL_MODULES
+from Grabber.modules import ALL_MODULES
 
 
 locks = {}
