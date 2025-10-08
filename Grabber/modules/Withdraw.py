cf21ab14ae803e485b6374ad79032a35348ab8d9
Shadow-rot <looktouhid@gmail.com>
2025-10-09 00:48:33 +0530

    Update Withdraw.py

--- Grabber/modules/Withdraw.py
+++ Grabber/modules/Withdraw.py
@@ -1,5 +1,5 @@
 from telegram.ext import CommandHandler
-from shivu import application, user_collection
+from Grabber import application, user_collection
 from telegram import Update
 from datetime import datetime, timedelta
 import asyncio
