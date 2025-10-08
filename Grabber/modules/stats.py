f66d1484feb2743c3ba9e81c843e142f186792f7
Shadow-rot <looktouhid@gmail.com>
2025-10-09 00:52:06 +0530

    Update stats.py

--- Grabber/modules/stats.py
+++ Grabber/modules/stats.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext
 
-from shivu import (application, PHOTO_URL, OWNER_ID,
+from Grabber import (application, PHOTO_URL, OWNER_ID,
                     user_collection, top_global_groups_collection, top_global_groups_collection, 
                     group_user_totals_collection)
 
