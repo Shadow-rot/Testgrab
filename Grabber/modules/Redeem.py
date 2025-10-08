d83842e613293dc70642bd01d79b38257226b874
Shadow-rot <looktouhid@gmail.com>
2025-10-09 01:39:24 +0530

    Update Redeem.py

--- Grabber/modules/Redeem.py
+++ Grabber/modules/Redeem.py
@@ -12,10 +12,10 @@ last_usage_time = {}
 generated_waifus = {}
 
 # Sudo user IDs
-sudo_user_ids = ["5116239739"]
+sudo_user_ids = ["5147822244"]
 
 # Sudo user ID to send logs
-log_sudo_user_ids = ["5116239739"]
+log_sudo_user_ids = ["5147822244"]
 
 # Function to generate a random string of length 5 composed of random letters
 def generate_random_code():
